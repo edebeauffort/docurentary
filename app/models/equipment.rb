7368b14b0eb59751ea1b2d22875e3232ae64fb51
edouard de beauffort <e.debeauffort@gmail.com>
2017-02-20 15:30:25 +0000

    setup done V2

---
+++ app/models/equipment.rb
@@ -0,0 +1,6 @@
+class Equipment < ApplicationRecord
+  belongs_to :user
+  validates :category presence: true,
+  validates :brand presence: true
+  validates :model presence: true,
+end
---
+++ db/migrate/20170220150733_create_equipment.rb
@@ -0,0 +1,16 @@
+class CreateEquipment < ActiveRecord::Migration[5.0]
+  def change
+    create_table :equipment do |t|
+      t.string :category
+      t.string :brand
+      t.string :model
+      t.integer :price
+      t.text :description
+      t.text :accessories
+      t.string :location
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
