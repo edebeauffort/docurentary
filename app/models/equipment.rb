fab9ba79c722fa7e0e8db91a96a16245889ecf9b
antonio lima <a.m.maiadelima@gmail.com>
2017-02-20 16:38:02 +0000

    validations work in progress

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,6 +1,30 @@
 class Equipment < ApplicationRecord
   belongs_to :user
-  validates :category presence: true,
-  validates :brand presence: true
-  validates :model presence: true,
+  geocoded_by :location
+  validates :category, :brand, :model, :price, :description, :location, presence: true
+  after_validation :geocode, :if => :location_changed?
 end
+
+
+
+
+  #create_table "equipment", force: :cascade do |t|
+    #t.string   "category"
+    #t.string   "brand"
+    #t.string   "model"
+    #t.integer  "price"
+    #t.text     "description"
+    #t.text     "accessories"
+    #t.string   "location"
+    #t.integer  "user_id"
+    #t.datetime "created_at",  null: false
+    #t.datetime "updated_at",  null: false
+    #t.index ["user_id"], name: "index_equipment_on_user_id", using: :btree
+  #end
+
+
+  #class Company < ActiveRecord::Base
+   # geocoded_by :address
+    #validates :address, presence: true
+    #after_validation :geocode, :if => :address_changed?
+  #end
