8591da8a85fd1dd1e19f73e4288ca38b6f8d8427
Nicolai Wiskemann <nicolai.wiskemann@gmail.com>
2017-02-21 12:56:03 +0000

    hello

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   resources :gadgets, only:[:show, :new, :create, :edit, :update, :destroy]
 
-  devise_for :users
-  root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+     root to: 'pages#home'
+
 end
