994e2823fab4c97c85c406d2423e3ec6fce0efd3
antonio lima <a.m.maiadelima@gmail.com>
2017-02-22 15:16:50 +0000

    google maps working on index page and localhost

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221135752) do
+ActiveRecord::Schema.define(version: 20170222122713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20170221135752) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_gadgets_on_user_id", using: :btree
   end
 
