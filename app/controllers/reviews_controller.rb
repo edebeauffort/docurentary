c2ed5c730d132f7e119b8451555ab06d6a224b0b
Nicolai Wiskemann <nicolai.wiskemann@gmail.com>
2017-02-22 17:37:46 +0000

    created review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+   def new
+    @gadget = Gadget.find(params[:gadget_id])
+    @review = Review.new
+  end
+  def create
+    @gadget = Gadget.find(params[:gadget_id])
+
+    if @gadget.reviews.create(reviews_params)
+      redirect_to gadget_path(@gadget)
+    else
+      render "new"
+    end
+  end
+
+  private
+  def reviews_params
+    params.require(:review).permit(:context, :rating)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222122713) do
+ActiveRecord::Schema.define(version: 20170222163218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,15 @@ ActiveRecord::Schema.define(version: 20170222122713) do
     t.index ["user_id"], name: "index_gadgets_on_user_id", using: :btree
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string   "context"
+    t.integer  "rating"
+    t.integer  "gadget_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["gadget_id"], name: "index_reviews_on_gadget_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -83,4 +92,5 @@ ActiveRecord::Schema.define(version: 20170222122713) do
   add_foreign_key "bookings", "gadgets"
   add_foreign_key "bookings", "users"
   add_foreign_key "gadgets", "users"
+  add_foreign_key "reviews", "gadgets"
 end
